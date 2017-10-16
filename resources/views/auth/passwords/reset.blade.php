a2597e45c6ba624697765aea107f8391ff42be0e
satish <satish@simplisticsolutions.in>
2017-10-16 14:49:40 +0530

    reset code

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,20 +1,14 @@
-<div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                 <h4 class="modal-title">Login/SignUp</h4>
-</div>
-
-<div class="modal-body">
-    @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
-<!--<div class="container">
+<div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Reset Password</div>
 
-                <div class="panel-body">-->
-                    <form class="form-horizontal" method="POST" action="{{ route('hp.public.password.request') }}">
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('password.request') }}">
                         {{ csrf_field() }}
 
                         <input type="hidden" name="token" value="{{ $token }}">
@@ -68,10 +62,9 @@
                             </div>
                         </div>
                     </form>
-             <!--   </div>
+            </div>
             </div>
         </div>
     </div>
-</div>-->
-@endsection
 </div>
+@endsection
