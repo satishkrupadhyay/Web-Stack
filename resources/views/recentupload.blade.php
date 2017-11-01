3724da8f951c1bec7bdeee4931d622e4cdef2ea0
satish <satish@simplisticsolutions.in>
2017-11-01 11:38:21 +0530

    update

--- resources/views/recentupload.blade.php
+++ resources/views/recentupload.blade.php
@@ -16,7 +16,7 @@
     <div class="row">
         <div class="col-md-4">
           <center> 
-            <img src="images/flier.jpg" alt="" style="width: 80%;"/>
+            <img src="images/Flier.jpg" alt="" style="width: 80%;"/>
                
           </center>  
         </div>
